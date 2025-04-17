e9abe4285afb9f4b2d093ddbbb21ad95fa9fc643
Shubham Kumar Gupta <gptshubham595@gmail.com>
2025-04-17 16:13:42 +0530

    init hilt, retrofit, glide, okhttp

--- app/secure-notes-android/app/build.gradle.kts
+++ app/secure-notes-android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    kotlin("android")
+    alias(libs.plugins.hilt.android.plugin)
+    alias(libs.plugins.ksp.plugin)
 }
 
 android {
@@ -27,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
@@ -45,4 +47,35 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.fragment)
+
+    implementation(libs.fragment.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.retrofit.gson)
+    implementation(libs.retrofit.scalars)
+    implementation(libs.logging.intercepter)
+    implementation(libs.okhttp)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.profileinstaller)
+
+    implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.glide)
+    ksp(libs.glide.ksp)
+
 }
--- app/secure-notes-android/app/src/main/java/com/shubham/securenotesandroid/presentation/MainActivity.kt
+++ app/secure-notes-android/app/src/main/java/com/shubham/securenotesandroid/presentation/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.shubham.securenotesandroid
+package com.shubham.securenotesandroid.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
+import com.shubham.securenotesandroid.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
